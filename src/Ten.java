5fb3b60d61d4750cb1645a3aed6f20926a960d84
Giuseppe Liguori <giuseppe@opensignal.com>
2017-12-12 18:40:18 +0000

    Empty files

---
+++ src/Ten.java
@@ -0,0 +1,23 @@
+/**
+ * Created by giuseppeliguori on 09/12/2017.
+ */
+public class Ten {
+    String input = "";
+    String inputeTest1 = "";
+    String inputeTest2 = "";
+    String inputeTest3 = "";
+    String inputeTest4 = "";
+
+    public Ten() {
+        partOne(inputeTest1);
+        partTwo(inputeTest1);
+    }
+
+    public void partOne(String input) {
+
+    }
+
+    public void partTwo(String input) {
+
+    }
+}
